3420eb0ae552a25d1466c2612acdbaca76ad8a9b
mmmsafry <mmmsafry@gmail.com>
2020-02-27 10:44:42 +0530

    Update AuthController.php
    this controller will return the response JSON like this 
    [
        {
            "auth": {
                "token_type": "Bearer",
                "expires_in": 1296000,
                "access_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIyIiwianRpIjoiMTdmODEwNGZjNGY4YjY4YTM5MjdkZDJhZTA0ZWQ2ZmQ4MGI1ZjYxMzE4ZTE1MTRhZTRmNjQzZWJmNWUwZWQzM2JlYWI2NzQ0ZTNkM2I5NmQiLCJpYXQiOjE1ODI3ODAxMzUsIm5iZiI6MTU4Mjc4MDEzNSwiZXhwIjoxNTg0MDc2MTM1LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.nCTRhbIkvjVh3YxJcZJFVKs4IfT3dpXGl9RJHYP3mJEHXyMqPoPLp948wLqDDLcTBZIvRLAkXd69p9vvcUYaJYtb2mIfp_p7NByVcprE9a-gNQV-DT-TIzqYAm_oVsCqp959_P2p6DN7wLFnVFjhP0kg3ZNYuCZO6tpH-dVfl0oTFNnJcBsOSuLHkXYqFgqrc40KgniY-ao8aYV0QeIU9K_mYYetx-LjUOqmu1qZ_bUj5ke7YEst9yO2vxf9Mco3GSzrrkhQhfI-FotLXWWJSIo2Lvo2M_RGVborBwUw2SC_b5llG_1TASsrxgoRBRaNgzjrS7woVuX0ehvfVyxUcMnHJiprEysnY4tDm5Q5DbLw7nc6BbtRN2kuTbyR1miQOAgo-H23vLb0xYqt4GfjKw5vLFbNUO1KLE8xvmdRxjh4Ht-wxYabcjdwvA2bOhUkkxNsKpN-W9IC526pIXxWwOw4A0_7jicTDkN8qP7GSOcQBw99ntxBh9kd9mK1EICInot7bZnpWxRDFEXlIOjTadvLYM4WRXh2DSkHAI2aEyqbEy-cKrBe9iMmwVUb-8KUeE08jQCkzFv9Zjd8CbAhfV8o4gUIS-8_xUV2FMgSSWXlDiVf5Da_GZUev7UIQIMoW263F65jGlc7GC72byCx3S8k8d4yRXEZAe8an57xtq4",
                "refresh_token": "def5020067ef9eeb110d9a4839e02eb4ce7819a2f01e673b3b0299b57b245439eec36bf7838139431a26502d94195271f910853df558b19433935149d431a9bd7e357c02e5343e84bded9728f9013045091cfbc9ef2d930032aaaa10872711bd917a9f39b0eb620c5cba42618ca35d4ca9e690cdddf36f6b77a23e3bafdce153162c1808d83720cd9df4cd6186f8c8141e16847bb81c72c4ba67c9ca883587fb4d498b67fb184573be98bd1e7ed8c35875e55dc4cb4e5f9e6a33f93de2c2507e00142d12f630d0b86d71a97824ce6343eb668db421efb5a7cb7bb02c2b861d192df074e3bd2e38c9cac44064693ae5bbbede46adf190ab82c099f8e3e48e4f9bca3e79bb4e763af0ea9f485d7145938afbd47179e154888846e3493bf23223de9384fed89cebe2061856d02a6ceadf34ae655c8aa8b8bb3ceab14ea3a565ebfe249f1b5c55613ef02d335fefd10af993608fd0ffeac514893b521c63d3323be1ef"
            },
            "user": {
                "id": 1,
                "name": "admin",
                "email": "admin@mail.com"
            }
        }
    ]

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-use App\Models\User;
+
+use App\Models\Users;
 use Exception;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use League\OAuth2\Server\Exception\OAuthServerException;
@@ -14,41 +14,84 @@ use Response;
 
 class AuthController extends \Laravel\Passport\Http\Controllers\AccessTokenController
 {
-    public function issueToken(ServerRequestInterface $request)
+    public function register(Request $request)
     {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:100',
+            'email' => 'required|string|email|max:200|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        $validatedData['password'] = bcrypt($request->password);
+
+        $user = User::create($validatedData);
+
+        $accessToken = $user->createToken('authToken')->accessToken;
+
+        return response(['user' => $user, 'access_token' => $accessToken]);
+    }
+
+    /**  @ToDo have to remove it later - practice */
+    public function login_no_need(Request $request)
+    {
+
+        $loginData = $request->validate([
+            'email' => 'email|required',
+            'password' => 'required'
+        ]);
+
+        if (!auth()->attempt($loginData)) {
+            return response(['message' => 'Invalid username or password']);
+        }
+
+        $accessToken = auth()->user()->createToken('authToken')->accessToken;
+
+        return response(['user' => auth()->user(), 'access_token' => $accessToken]);
+    }
+
+    public function login(ServerRequestInterface $request)
+    {
+
         try {
             //get username (default is :email)
             $username = $request->getParsedBody()['username'];
 
             //get user
-            $user = User::where('email', '=', $username)->firstOrFail();
+            $user = Users::where('email', '=', $username)->firstOrFail();
 
             //issuetoken
             $tokenResponse = parent::issueToken($request);
 
+
             //convert response to json string
-            $content = $tokenResponse->getBody()->__toString();
+            $content = $tokenResponse->getContent();
 
             //convert json to array
-            $data = json_decode($content, true);
 
-            if(isset($data["error"]))
+            $data = json_decode($content, true);
+            /** @ToDo not working - */
+            if (isset($data["error"]))
                 throw new OAuthServerException('The user credentials were incorrect.', 6, 'invalid_credentials', 401);
 
             //add access token to user
             $user = collect($user);
-            $user->put('access_token', $data['access_token']);
+            $output['auth'] = $data;
+            $output['user'] = $user;
 
-            return Response::json(array($user));
-        }
-        catch (ModelNotFoundException $e) { // email notfound
+            return Response::json(array($output));
+        } catch (ModelNotFoundException $e) { // email notfound
+            return 'ModelNotFoundException';
             //return error message
-        }
-        catch (OAuthServerException $e) { //password not correct..token not granted
+        } catch (OAuthServerException $e) { //password not correct..token not granted
             //return error message
-        }
-        catch (Exception $e) {
+            return 'OAuthServerException';
+        } catch (Exception $e) {
             ////return error message
+            $data['error'] = 'Authentication fail';
+            $data['description'] = 'Invalid username or password';
+            $data['message'] = 'Provided username or password invalid';
+            return response($data, 401);
+
         }
     }
 }
